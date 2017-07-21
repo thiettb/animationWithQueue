3e2790981e657c32199e06b68353ebb79c969c67
ThietTB <ThietTB@MacBook-Pro-cua-Macbook.local>
2017-07-21 10:42:27 +0700

    Update ViewController and Readme

--- DemoAnimation/ViewController.swift
+++ DemoAnimation/ViewController.swift
@@ -84,7 +84,33 @@ class ViewController: UIViewController {
 //    }
 //
 //}
-queue
+public struct Queue<T> {
+    fileprivate var array = [T]()
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    public mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+    
+    public mutating func dequeue() -> T? {
+        if isEmpty {
+            return nil
+        } else {
+            return array.removeFirst()
+        }
+    }
+    
+    public var front: T? {
+        return array.first
+    }
+}
 
 
 
